6dd4a0bb97577616024ef2a4bf71b381e9176b04
coolatent <62825067+coolatent@users.noreply.github.com>
2021-03-23 19:04:01 -0400

    commit main method class and shop item class

---
+++ Game/src/game/ShopItem.java
@@ -0,0 +1,12 @@
+package game;
+
+public class ShopItem {
+
+    Weapon item;
+    int numberInStock;
+
+    public ShopItem(Weapon w, int nInStock) {
+        item = w;
+        numberInStock = nInStock;
+    }
+}
