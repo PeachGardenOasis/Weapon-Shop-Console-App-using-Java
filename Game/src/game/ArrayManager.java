b9b764e336f6e5111bf8613f88212b1c53580e78
Chi Calvin Nguyen <Cn512@live.com>
2021-04-03 14:02:30 -0400

    Modified: ArrayManager get() method & QuadraticProbing get() method TO DO: Add delete() method

--- Game/src/game/ArrayManager.java
+++ Game/src/game/ArrayManager.java
@@ -53,27 +53,45 @@ public class ArrayManager {
 
         }
 
-
+        // MODIFIED: Checks if item exists in array using hash function and returns it
         public ShopItem get(String key)
         {
-            int location = 0; //gets location in table based on key
+            ShopItem toReturn = qp.get(table, key);
+            return toReturn;
             
-            while (location <numItems && key.compareTo(table[location].item.weaponName) != 0)
-            {  // not empty and not item
-                location++;
+            /* LEGACY
+            // Lowers complexity of hash function for faster get method
+            int count = 1;
+            int startLoc = qp.quadhashFunction2(key); // gets location in table based on key
+            int loc = startLoc;
+            
+            // Exits when it finds an empty location or the matching item
+            while (table[loc] != null && table[loc].item.weaponName.compareTo(key) != 0){
+                loc = (startLoc + (count * count) % maxItems);
+                count++;
             }
-            if (location<numItems){
-                return table[location];
+            
+            // If the location's element matches the item, return the item
+            if (table[loc] != null && table[loc].item.weaponName.compareTo(key) == 0){
+                return table[loc];
             }
+            
             return null;
+            */
+        }
+        
+        // TO DO: DELETE FUNCTION
+        public void delete(String key){
+            
         }
 
         public void printTable()
         {
             int count = 0;
             for (int x = 0; x < maxItems; x++)
-            {
-                if(table[x] != null){
+            {   
+                // Lists all weapons in the shop that are in stock (greater than 0)
+                if(table[x] != null && table[x].numberInStock > 0){
                     System.out.println("Name: " +table[x].item.weaponName+"   Damage:"+table[x].item.damage+"    Cost:"+table[x].item.cost+"     Quantity in stock:"+table[x].numberInStock);
                 }
             }
--- Game/src/game/QuadraticProbing.java
+++ Game/src/game/QuadraticProbing.java
@@ -19,16 +19,32 @@ public class QuadraticProbing {
         return value % tableSize;
     }
     
+    private int quadhashFunction2(String w){
+        int value = 0, weight = 1;
+        
+        for (int i = 0; i < w.length(); i++){
+            value += (w.charAt(i)-'a'+1)*weight;
+            weight++;
+        }
+        
+        value *= weight;
+        return value % tableSize;
+    }
+    
     public void add(ShopItem[] table, Weapon item, int quantity){
+        
+        // Hash Function Location
         int count = 1;
-        int startLoc = quadhashFunction(item);
+        int startLoc = quadhashFunction2(item.weaponName);
         int loc = startLoc;
         
+        // Quadratic probing, finds empty location or location of matching item
         while (table[loc] != null && table[loc].item.weaponName.compareTo(item.weaponName) != 0){  
             loc = (startLoc + (count * count) % tableSize);
             count++;
         }
         
+        // Checks if item at location has matching name, adds stock if true
         if (table[loc] != null && table[loc].item.weaponName.compareTo(item.weaponName) == 0){
             table[loc].numberInStock = table[loc].numberInStock + quantity;
             return;
@@ -36,5 +52,26 @@ public class QuadraticProbing {
         
         table[loc] = new ShopItem(item,quantity);
         return;
+    }
+    
+    public ShopItem get(ShopItem[] table, String key){
+        
+        // Lowers complexity of hash function for faster get method
+        int count = 1;
+        int startLoc = quadhashFunction2(key); // gets location in table based on key
+        int loc = startLoc;
+
+        // Exits when it finds an empty location or the matching item
+        while (table[loc] != null && table[loc].item.weaponName.compareTo(key) != 0){
+            loc = (startLoc + (count * count) % tableSize);
+            count++;
+        }
+
+        // If the location's element matches the item, return the item
+        if (table[loc] != null && table[loc].item.weaponName.compareTo(key) == 0){
+            return table[loc];
+        }
+
+        return null;
     }
 }
